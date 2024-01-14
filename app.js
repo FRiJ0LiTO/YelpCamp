0142ad29ae50168331a95c96d34763e35465b279
FRiJ0LITO <a00835376@tec.mx>
2024-01-14 17:38:27 -0600

    Campground Index

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const Campground = require('./models/campground')
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Databased connected")
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Databased connected')
 })
 
 const app = express();
@@ -17,17 +17,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render("index")
+    res.render('index')
 })
 
-app.get('/make', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
 })
 
 app.listen(3000, () => {
-    console.log("SERVER RUNNING AT PORT 3000")
+    console.log('SERVER RUNNING AT PORT 3000');
 })
 
 
