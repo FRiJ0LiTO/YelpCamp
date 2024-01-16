77d8ae71eb135bfb526f31c0d81a64faa9c5c13e
FRiJ0LITO <a00835376@tec.mx>
2024-01-16 12:19:36 -0600

    Campground Show, New & Create

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground')
+const Campground = require('./models/campground');
+const { clearCache } = require('ejs');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -15,14 +16,32 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', { campgrounds })
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    console.log(req.body);
+    const newCampground = new Campground(req.body);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
 })
 
 app.listen(3000, () => {
